2cbdd4403a54634db38270b6e0429c9d6ce071df
UsernameL56 <corsi.lucasgabriel.studente@itispaleocapa.it>
2025-01-16 09:40:44 +0100

    Aggiungere i file di progetto.

---
+++ Gestionale_Biblioteca/Classes/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Gestionale_Biblioteca.Classes
+{
+    public class Database
+    {
+        private MySqlConnection connection;
+        private string connectionString = "Server=corsilg.mysql.altervista.org;Database=my_corsilg;Uid=corsilg;Pwd=;";
+
+        public MySqlConnection GetConnection()
+        {
+            if (connection == null)
+            {
+                connection = new MySqlConnection(connectionString);
+            }
+            return connection;
+        }
+    }
+}
